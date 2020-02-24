787e5abd0b1b1604c306d05bae7e8188a9e0ba33
Alex Burley <alex.burley@tray.io>
2020-02-24 12:56:03 +0000

    Actually fix lint

--- test/unit/connection/test-connection-ssl-max-tls1.0-reject.js
+++ test/unit/connection/test-connection-ssl-max-tls1.0-reject.js
@@ -3,8 +3,8 @@ var common     = require('../../common');
 var connection = common.createConnection({
   port : common.fakeServerPort,
   ssl  : {
-    ca: common.getSSLConfig().ca,
-    maxVersion: 'TLSv1'
+    ca         : common.getSSLConfig().ca,
+    maxVersion : 'TLSv1'
   }
 });
 
@@ -19,7 +19,7 @@ server.listen(common.fakeServerPort, function(err) {
 
   connection.ping(function(err) {
     var NODE_MAJOR_VERSION = process.versions.node.split('.')[0];
-    if (!err && NODE_MAJOR_VERSION >= 10) assert.fail("Expected to fail due to mismatched TLS versions for Node12+")
+    if (!err && NODE_MAJOR_VERSION >= 10) assert.fail('Expected to fail due to mismatched TLS versions for Node12+');
     connection.destroy();
     server.destroy();
   });
--- test/unit/connection/test-connection-ssl-min-tls1.2-reject.js
+++ test/unit/connection/test-connection-ssl-min-tls1.2-reject.js
@@ -3,8 +3,8 @@ var common     = require('../../common');
 var connection = common.createConnection({
   port : common.fakeServerPort,
   ssl  : {
-    ca: common.getSSLConfig().ca,
-    minVersion: 'TLSv1.2'
+    ca         : common.getSSLConfig().ca,
+    minVersion : 'TLSv1.2'
   }
 });
 
@@ -18,7 +18,7 @@ server.listen(common.fakeServerPort, function(err) {
   var NODE_MAJOR_VERSION = process.versions.node.split('.')[0];
   if (err) throw err;
   connection.ping(function(err) {
-    if (!err && NODE_MAJOR_VERSION >= 10) assert.fail("Expected to fail due to mismatched TLS versions for Node12+")
+    if (!err && NODE_MAJOR_VERSION >= 10) assert.fail('Expected to fail due to mismatched TLS versions for Node12+');
     connection.destroy();
     server.destroy();
   });
